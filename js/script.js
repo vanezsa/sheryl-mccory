9676983938fab0dded172cce6ea77d64b8157fb1
Vanessa <cup0ft3a12@gmail.com>
2018-06-08 19:30:20 +0800

    make responsive

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    animateOut: 'fadeOut',
+    autoplay:true,
+    autoHeight:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+});
